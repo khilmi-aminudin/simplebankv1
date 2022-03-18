98a7741bdf88ad0c7d0a60d143f257f430f0f107
khilmi aminudin <khilmiaminudin715@gmail.com>
2022-03-18 16:11:09 +0700

    fix simplebank.sql schema

--- simplebank.sql
+++ simplebank.sql
@@ -7,16 +7,16 @@ CREATE TABLE "accounts" (
 );
 
 CREATE TABLE "entries" (
-  "id" bigint PRIMARY KEY,
-  "account_id" bigint,
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "transfers" (
-  "id" bigint PRIMARY KEY,
-  "from_account_id" bigint,
-  "to_account_id" bigint,
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
